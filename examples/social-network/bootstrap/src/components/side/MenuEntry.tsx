488555d431a2ce170de3fa97618ebe96448a6236
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2023-01-22 20:42:59 +0000

    BS-SN: Improve Sidebar

--- examples/social-network/bootstrap/src/components/side/MenuEntry.tsx
+++ examples/social-network/bootstrap/src/components/side/MenuEntry.tsx
@@ -6,10 +6,7 @@ export const MenuEntry = (props: {
 }): JSX.Element => {
   return (
     <h4 class="mt-4">
-      <a
-        href="#"
-        class="fw-bold text-light font-monospace text-uppercase text-decoration-none"
-      >
+      <a href="#" class="fw-bold text-light text-decoration-none">
         <i class={`fa-solid ${props.icon}`}></i>
         <span class="d-none d-sm-inline"> {props.label}</span>
       </a>
