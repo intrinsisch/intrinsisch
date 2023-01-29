46238666b724a26dbf1c80e1d711e66c21489125
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2023-01-29 20:31:44 +0000

    Refactor parse function

---
+++ measure/lib/parse.ts
@@ -0,0 +1,54 @@
+import { parse as parseCSSFromString, AST } from "https://deno.land/x/css@0.3.0/mod.ts";
+import {
+  DOMParser,
+  HTMLDocument
+} from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
+import { Project } from "./config.ts";
+
+export async function parseProject(p: Project): Promise<{ html: HTMLDocument, css: AST, p: Project } | undefined> {
+  const htmlPromise = parseHTML(p);
+  const cssPromise = parseCSS(p);
+
+  const [html, css] = await Promise.all([htmlPromise, cssPromise]);
+
+  if (html === undefined) {
+    return;
+  }
+  if (css === undefined) {
+    return;
+  }
+  return { html, css, p }
+}
+
+async function parseHTML(p: Project): Promise<HTMLDocument | undefined> {
+  const projectPath = ["./tmp", p.short].join("/");
+  for await (const entry of Deno.readDir(projectPath)) {
+    if (entry.isFile && entry.name.endsWith(".html")) {
+      const path = [projectPath, entry.name].join("/");
+      const file = await Deno.readFile(path);
+      const decoder = new TextDecoder("utf-8");
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(decoder.decode(file), "text/html");
+      if (doc === null) {
+        console.error("HTML couldn't get parsed.", p);
+        return;
+      }
+      return doc;
+    }
+  }
+  console.error("No HTML found.", projectPath, p);
+}
+
+
+async function parseCSS(p: Project): Promise<AST | undefined> {
+  const projectPath = ["./tmp", p.short, "assets"].join("/");
+  for await (const entry of Deno.readDir(projectPath)) {
+    if (entry.isFile && entry.name.endsWith(".css")) {
+      const path = [projectPath, entry.name].join("/");
+      const file = await Deno.readFile(path);
+      const decoder = new TextDecoder("utf-8");
+      return parseCSSFromString(decoder.decode(file));
+    }
+  }
+  console.error("No CSS found.", projectPath, p);
+}
--- measure/script.ts
+++ measure/script.ts
@@ -1,13 +1,8 @@
-
 import { projects } from "./lib/config.ts";
 import { cleanup } from "./lib/cleanup.ts";
 import { setup } from "./lib/setup.ts";
-import { HTMLParsed, parseHTML } from "./lib/parseHTML.ts";
-import { CSSParsed, parseCSS } from "./lib/parseCSS.ts";
+import { parseProject } from "./lib/parse.ts";
 
 await cleanup();
 await Promise.all(projects.map(setup));
-await Promise.all(projects.map(parseHTML))
-  .then((d) => d.filter((it): it is Required<HTMLParsed> => it.doc !== undefined));
-await Promise.all(projects.map(parseCSS))
-  .then((d) => d.filter((it): it is Required<CSSParsed> => it.ast !== undefined));
+await Promise.all(projects.map(parseProject));
