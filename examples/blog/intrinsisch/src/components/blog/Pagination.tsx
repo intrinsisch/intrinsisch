b6e97274fa019060f2beb27c5520d705a8ca1167
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2023-03-01 22:34:56 +0000

    pagination

--- examples/blog/intrinsisch/src/components/blog/Pagination.tsx
+++ examples/blog/intrinsisch/src/components/blog/Pagination.tsx
@@ -2,30 +2,30 @@ import type { JSX } from "solid-js";
 
 export const Pagination = (): JSX.Element => {
   return (
-    <nav aria-label="Page navigation example">
-      <ul class="cluster unlist center">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
+    <nav class="pagination" aria-label="Page navigation example">
+      <ul class="cluster unlist center" style="--spa--spacing:0;">
+        <li>
+          <a class="pagination-item no-anchor" href="#" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link active" href="#">
+        <li>
+          <a class="pagination-item no-anchor active" href="#">
             1
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link" href="#">
+        <li>
+          <a class="pagination-item no-anchor" href="#">
             2
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link" href="#">
+        <li>
+          <a class="pagination-item no-anchor" href="#">
             3
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
+        <li>
+          <a class="pagination-item no-anchor" href="#" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
           </a>
         </li>
