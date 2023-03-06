2e6dd0829c408a22749e3712c2eb8ead42a12e45
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2023-03-06 13:15:51 +0000

    Improve BS SN example

--- examples/social-network/bootstrap/src/components/main/Feed.tsx
+++ examples/social-network/bootstrap/src/components/main/Feed.tsx
@@ -4,10 +4,12 @@ import { feedlist } from "./Feedlist";
 
 export const Feed = (): JSXElement => {
   return (
-    <div class="d-flex flex-column gap-3 mt-5">
+    <div class="row pt-3 g-5">
       <For each={feedlist}>
         {(f, i) => (
-          <Feedcard entry={f} index={i()} />
+          <div class="col-md-6">
+            <Feedcard entry={f} index={i()} />
+          </div>
         )}
       </For>
     </div>
--- examples/social-network/bootstrap/src/components/main/Main.tsx
+++ examples/social-network/bootstrap/src/components/main/Main.tsx
@@ -1,14 +1,12 @@
-import type { JSXElement} from "solid-js";
+import type { JSXElement } from "solid-js";
 import { Feed } from "./Feed";
 import { Stories } from "./Stories";
 
 export const Main = (): JSXElement => {
   return (
-    <main class="py-3 px-4 row">
-      <div class="col-md-10 col-lg-6 col-xl-4">
-        <Stories />
-        <Feed />
-      </div>
+    <main class="py-3 pe-4 container">
+      <Stories />
+      <Feed />
     </main>
   );
 };
