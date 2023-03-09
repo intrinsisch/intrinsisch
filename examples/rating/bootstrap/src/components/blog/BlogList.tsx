3a4baf1477105affd883ca1d75f4284ba7beba8f
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2023-03-09 18:49:46 +0000

    Remove pagination

--- examples/rating/bootstrap/src/components/blog/BlogList.tsx
+++ examples/rating/bootstrap/src/components/blog/BlogList.tsx
@@ -1,14 +1,12 @@
 import { For, JSXElement } from "solid-js";
 import { blogEntries } from "./BlogData";
 import { BlogEntry } from "./BlogEntry";
-import { Pagination } from "./Pagination";
 
 export const BlogList = (): JSXElement => {
   return (
     <>
       <h1>Latest posts</h1>
       <For each={blogEntries}>{(entry) => <BlogEntry {...entry} />}</For>
-      <Pagination />
     </>
   );
 };
