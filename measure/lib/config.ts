128df65fb8dff18e65a88c07058f0dd3a5359d29
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2023-03-13 21:29:47 +0000

    Update script

--- measure/lib/config.ts
+++ measure/lib/config.ts
@@ -1,12 +1,14 @@
 export const projects = [
-  { path: "base/bootstrap", label: "Bootstrap: Basis", short: "base-bs" },
-  { path: "base/feo", label: "Feo: Basis", short: "base-feo" },
-  { path: "rating/bootstrap", label: "Bootstrap: Rating Komponente", short: "rating-bs" },
-  { path: "rating/feo", label: "Feo: Rating Komponente", short: "rating-feo" },
-  { path: "chat/bootstrap", label: "Bootstrap: Chat Komponente", short: "chat-bs" },
-  { path: "chat/feo", label: "Feo: Chat Komponente", short: "chat-feo" },
-  { path: "stat/bootstrap", label: "Bootstrap: Stat Komponente", short: "stat-bs" },
-  { path: "stat/feo", label: "Feo: Stat Komponente", short: "stat-feo" },
+  { path: "base/bootstrap", label: "Responsive: Basisprojekt", short: "base-bs" },
+  { path: "base/feo", label: "Intrinsic: Basisprojekt", short: "base-feo" },
+  { path: "rating/bootstrap", label: "Responsive: Rating Komponente", short: "rating-bs" },
+  { path: "rating/feo", label: "Intrinsic: Rating Komponente", short: "rating-feo" },
+  { path: "chat/bootstrap", label: "Responsive: Chat Komponente", short: "chat-bs" },
+  { path: "chat/feo", label: "Intrinsic: Chat Komponente", short: "chat-feo" },
+  { path: "stat/bootstrap", label: "Responsive: Stat Komponente", short: "stat-bs" },
+  { path: "stat/feo", label: "Intrinsic: Stat Komponente", short: "stat-feo" },
+  { path: "all/bootstrap", label: "Responsive: Alle Komponenten", short: "all-bs" },
+  { path: "all/feo", label: "Intrinsic: Alle Komponenten", short: "all-feo" },
 ];
 
 export type Project = (typeof projects)[number];
--- measure/lib/utils/latexUtils.ts
+++ measure/lib/utils/latexUtils.ts
@@ -12,6 +12,11 @@ export type FinalValues = {
   ncrb: string;
 }
 
+type BaseValues = {
+  bootstrap?: Partial<FinalValues>,
+  feo?: Partial<FinalValues>
+}
+
 export function prettyPrint(finalValues: FinalValues[]) {
   console.log();
   console.log();
@@ -19,11 +24,46 @@ export function prettyPrint(finalValues: FinalValues[]) {
   console.log();
 
   let counter = 0;
-  finalValues.forEach(({ label, u, s, a }) => {
+  const base: BaseValues = {};
+
+  finalValues.forEach(({ label, u, s }) => {
     if (counter % 2 === 0) {
       console.log("\\hline\\hline");
     }
-    console.log(`${label} & ${u} & ${s} & ${a} \\\\`);
+    console.log(`${label} & ${u} & ${s} \\\\`);
+    counter++;
+  });
+
+  console.log();
+  console.log();
+  log("Wartbarkeit 2");
+  console.log();
+
+  counter = 0;
+  finalValues.forEach(({ label, a }) => {
+    if (label === "Responsive: Basisprojekt") {
+      base.bootstrap = { ...base.bootstrap, a }
+    }
+    if (label === "Intrinsic: Basisprojekt") {
+      base.feo = { ...base.feo, a }
+    }
+
+    if (label.endsWith("Basisprojekt")) {
+      return;
+    }
+
+    if (counter % 2 === 0) {
+      console.log("\\hline\\hline");
+    }
+
+    const isFeo = label.startsWith("Intr");
+    const absolute: string = label.endsWith("Basisprojekt")
+      ? ""
+      : isFeo
+        ? (Number(a) - Number(base.feo?.a)).toFixed(2)
+        : (Number(a) - Number(base.bootstrap?.a)).toFixed(2);
+
+    console.log(`${label} & ${a} & ${absolute} \\\\`);
     counter++;
   });
 
@@ -35,10 +75,68 @@ export function prettyPrint(finalValues: FinalValues[]) {
 
   counter = 0;
   finalValues.forEach(({ label, rl, norb, nerb, nadrb, ncrb }) => {
+    if (label === "Responsive: Basisprojekt") {
+      base.bootstrap = { ...base.bootstrap, rl, norb, nerb, nadrb, ncrb }
+    }
+    if (label === "Intrinsic: Basisprojekt") {
+      base.feo = { ...base.feo, rl, norb, nerb, nadrb, ncrb }
+    }
+
     if (counter % 2 === 0) {
       console.log("\\hline\\hline");
     }
     console.log(`${label} & ${rl} & ${norb} & ${nerb} & ${nadrb} & ${ncrb} \\\\`);
+    counter++;
+
+  });
+
+  console.log();
+  console.log();
+  log("Komplexität 2");
+  console.log();
+
+  counter = 0;
+  finalValues.forEach(({ label, rl, norb, nerb, nadrb, ncrb }) => {
+    if (label.endsWith("Basisprojekt")) {
+      return;
+    }
+    if (counter % 2 === 0) {
+      console.log("\\hline\\hline");
+    }
+
+    const isFeo = label.startsWith("Intr");
+    const absoluteRL: string = label.endsWith("Basisprojekt")
+      ? ""
+      : isFeo
+        ? (Number(rl) - Number(base.feo?.rl)).toFixed(0)
+        : (Number(rl) - Number(base.bootstrap?.rl)).toFixed(0);
+
+    const absoluteNORB: string = label.endsWith("Basisprojekt")
+      ? ""
+      : isFeo
+        ? (Number(norb) - Number(base.feo?.norb)).toFixed(0)
+        : (Number(norb) - Number(base.bootstrap?.norb)).toFixed(0);
+
+    const absoluteNERB: string = label.endsWith("Basisprojekt")
+      ? ""
+      : isFeo
+        ? (Number(nerb) - Number(base.feo?.nerb)).toFixed(0)
+        : (Number(nerb) - Number(base.bootstrap?.nerb)).toFixed(0);
+
+    const absoluteNADRB: string = label.endsWith("Basisprojekt")
+      ? ""
+      : isFeo
+        ? (Number(nadrb) - Number(base.feo?.nadrb)).toFixed(2)
+        : (Number(nadrb) - Number(base.bootstrap?.nadrb)).toFixed(2);
+
+    const absoluteNCRB: string = label.endsWith("Basisprojekt")
+      ? ""
+      : isFeo
+        ? (Number(ncrb) - Number(base.feo?.ncrb)).toFixed(0)
+        : (Number(ncrb) - Number(base.bootstrap?.ncrb)).toFixed(0);
+
+    console.log(`${label} & ${absoluteRL} & ${absoluteNORB} & ${absoluteNERB} & ${absoluteNADRB} & ${absoluteNCRB} \\\\`);
+
     counter++;
   });
 
